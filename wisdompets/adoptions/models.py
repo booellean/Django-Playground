054b5b35da21127330b5d382b37662a9eaf10055
booellean <booelle.commissions@gmail.com>
2019-06-22 13:41:50 -0700

    HARD FIX

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
 class Pet(models.Model):
-  SEX_CHOICES = [('M', 'Male'), ('F', 'Female'), ('O', 'Other')]
-
-  name = models.CharField(max_length=100)
-  submitter = models.CharField(max_length=100)
-  species = models.CharField(max_length=100)
-  breed = models.CharField(max_length=100, blank=True)
-  description = models.TextField()
-  sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
-  submission_date = models.DateTimeField()
-  age = models.IntegerField(null=True)
-  vaccinations = models.ManyToManyField('Vaccine', blank=True)
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
-  name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
